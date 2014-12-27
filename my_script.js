a33062e9b5232b944e0ace3886de52e45ddd59ca
amitwedig <amit.kumar@wedigtech.com>
2014-12-27 18:17:36 +0530

    my_script style content updated

--- my_script.js
+++ my_script.js
@@ -1,5 +1,5 @@
 $("body").append( "<div class='show_alert'>Show Alert Box</div>" );
-$(".show_alert").attr( "style", "position:fixed; bottom:0px; background:#f00; padding:5px; color:#fff; cursor:pointer; font-weight:bold; right:0px;" );
+$(".show_alert").attr( "style", "position:fixed; bottom:0px; background:#f00; padding:5px; color:#fff; cursor:pointer; font-weight:bold; font-size:12px; right:0px;" );
 $(".show_alert").click(function() {
 	alert( "My first app alert box." );
 });
