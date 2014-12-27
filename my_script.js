c990c905ed85cc8578e204e79910a47054a583f3
amitwedig <amit.kumar@wedigtech.com>
2014-12-27 18:06:30 +0530

    my_script updated

--- my_script.js
+++ my_script.js
@@ -1,1 +1,5 @@
-alert("testing app successfull.");
+$("body").append( "<div class='show_alert'>Show Alert Box</div>" );
+$(".show_alert").attr( "style", "position:absolute; bottom:0px; background:#f00; padding:5px; color:#fff;" );
+$(".show_alert").click(function() {
+	alert( "My first app alert box." );
+});
